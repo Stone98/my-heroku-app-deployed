2135ef14e1bbaafe93d6096a3b7ebfaea5624d0e
Stone98 <stone18003@gamil.com>
2021-04-08 09:58:40 -0700

    third push

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
-const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
